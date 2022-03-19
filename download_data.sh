17c942db925360247d6003cd07c14dc3049f852c
Daniele Di Pompeo <dipompeodaniele@gmail.com>
2022-03-19 14:36:51 +0100

    update download script

--- download_data.sh
+++ download_data.sh
@@ -3,4 +3,8 @@
 HOST=$1
 EXPERIMENT_NAME=$2
 
-rsync -aPr --mkpath $HOST:$(dirname `grep -oP "/mnt.+run$i" ${EXPERIMENT_NAME}/README.md`) ${EXPERIMENT_NAME}'/data/'
+rsync -aPr --exclude=".*" --mkpath $HOST:$(dirname `grep -oP "/mnt.+run$i" ${EXPERIMENT_NAME}/README.md`) ${EXPERIMENT_NAME}'/data/'
+
+git add ${EXPERIMENT_NAME}'/data/'
+git commit -m "add ${EXPERIMENT_NAME} data"
+git push
